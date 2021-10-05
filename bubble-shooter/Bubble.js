5fe44a66781438813329771c8e21e7b705da4679
David-Akpan <david3emmanuel@gmail.com>
2021-10-05 08:54:55 +0100

    added color

--- bubble-shooter/Bubble.js
+++ bubble-shooter/Bubble.js
@@ -2,9 +2,12 @@ class Bubble {
   constructor(i,j) {
     this.i = i;
     this.j = j;
+    this.c = color(random(255),random(255),random(255));
   }
   
   show() {
+    noStroke();
+    fill(this.c);
     ellipseMode(CORNER);
     ellipse(this.j*scl,this.i*scl,scl,scl);
   }
